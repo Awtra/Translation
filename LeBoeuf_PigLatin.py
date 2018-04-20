900094d94a4408f726a8adf9bb740032527c7b2b
Ali <aliimody1@gmail.com>
2018-04-20 08:23:52 -0400

    Add files via upload

---
+++ LeBoeuf_PigLatin.py
@@ -0,0 +1,12 @@
+# Austin LeBoeuf Period 1
+# this program converts a word to pig latin
+
+word = ""
+
+while word != "stop":
+    word = input("Enter a word that will be translated.")
+    print(word[1:] + word[0] + "ay")
+   
+   
+            
+  
